c088812f3fc00a782d294e2c3bc32337bada92ee
abcfhuang <abcfinsomnia@gmail.com>
2015-05-10 17:29:14 +0800

    示範 ASP.NET MVC 5.1 的 Enum 相關功能

---
+++ MVC5Course/Models/RegViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models
+{
+    public enum MemberLevel
+    {
+        User,
+        Admin
+    }
+
+    public class RegViewModel
+    {
+        public string UserID { get; set; }
+        public string UserPW { get; set; }
+
+        [UIHint("Enum-radio")]
+        public MemberLevel Level { get; set; }
+    }
+}
